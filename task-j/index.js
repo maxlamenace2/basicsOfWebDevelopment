e36eeee8ed37987327fe32a49a4f17b3144c0896
maxlamenace2 <maxence.gautier.grall@gmail.com>
2025-11-29 18:39:46 +0200

    update link

--- task-j/index.js
+++ task-j/index.js
@@ -5,4 +5,12 @@ document.addEventListener("DOMContentLoaded", () => {
   toggle.addEventListener('click', () => {
     nav.classList.toggle('active');
   });
+  const buttons = document.querySelectorAll('.card-btn');
+    buttons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            window.location.href = 'https://maxlamenace2.github.io/basicsOfWebDevelopment/task-k/order.html'; // Remplace par le lien de ta page de commande
+        });
+    });
 });
+
+
