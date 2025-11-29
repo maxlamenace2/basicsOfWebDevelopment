f04be80405c5937e1fdf21e44007b7a73382c33e
maxlamenace2 <maxence.gautier.grall@gmail.com>
2025-11-29 18:54:06 +0200

    upadate

--- task-j/index.js
+++ task-j/index.js
@@ -1,16 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Hamburger menu
   const toggle = document.querySelector('.menu-toggle');
   const nav = document.querySelector('header nav');
 
   toggle.addEventListener('click', () => {
     nav.classList.toggle('active');
   });
-  const buttons = document.querySelectorAll('.card-btn');
-    buttons.forEach(btn => {
-        btn.addEventListener('click', () => {
-            window.location.href = 'https://maxlamenace2.github.io/basicsOfWebDevelopment/task-k/order.html'; // Remplace par le lien de ta page de commande
-        });
-    });
+  
+// Boutons "Rent This"
+  const buttons = document.querySelectorAll('.btn.card-btn');
+  buttons.forEach(btn => {
+      btn.addEventListener('click', () => {
+          window.location.href = 'https://maxlamenace2.github.io/basicsOfWebDevelopment/task-k/order.html';
+      });
+  });
+  
 });
-
-
